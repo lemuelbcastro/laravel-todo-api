384b03eb395a5a232a7ac0621fb6dcbdb9254f4a
Lemuel Castro <lemuelbercasiocastro@gmail.com>
2020-09-23 21:01:57 +0800

    Implement Sanctum token based authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\User as UserResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'LoginController@authenticate');
+
+Route::group([
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::get('/user', function (Request $request) {
+        return new UserResource($request->user());
+    });
 });
